30558081c53f6334950cf316493c4f5e0e956470
IrishPhone <57331045+IrishPhone@users.noreply.github.com>
2019-11-14 00:15:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ function resetBars() {
 }
 
 function buttonPress(btn) {
-	let val = parseInt( $(`div#${btn.target.id}`).html() );
+	let val = parseInt( $(".progress-bar").html() );
 	let addPct = 0;
 	switch(btn.target.id) {
-		case "group1": addPct = 1; break;
-		case "group2": addPct = 3; break;
-		case "group3": addPct = 7; break;
+		case "button1": addPct = 1; break;
+		case "button2": addPct = 3; break;
+		case "button3": addPct = 7; break;
 		default: addPct = 0;
 	}
 	val = addPct + val <= 100 ? addPct + val : 100 ;
 
 	
-	$(`div#${btn.target.id}`).css("width", val+"%");
+	$(".progress-bar").css("width", val+"%");
 
-	$(`div#${btn.target.id}`).html(val+"%");
+	$(".progress-bar").html(val+"%");
 }
 
 function main() {
